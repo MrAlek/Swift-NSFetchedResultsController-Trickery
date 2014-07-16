2633a07eff6bab6ef319e69fff780eccfaabfdb3
Alek Åström <alek.astrom@gmail.com>
2014-07-16 19:04:08 +0200

    Initial Commit

---
+++ CoreDataTrickerySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTrickerySwift
+//
+//  Created by Alek Astrom on 2014-07-16.
+//  Copyright (c) 2014 Apps and Wonders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
